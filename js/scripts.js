01c2efb29313429451e5992c0e64aeedd7c92aaa
alessiovietri <info@alessiovietri.it>
2024-01-08 12:30:14 +0100

    Completato LC

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,22 @@
-// Test funzionamento
-console.log('Template HTML CSS JS');
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: '45 Vue cicli',
+            spesa: ['mele', 'pere', 'mandarinE'],
+            newElem: ''
+        };
+    },
+    methods: {
+        addToList() {
+            if (this.newElem.length > 0) {
+                console.log(this.newElem);
+
+                this.spesa.push(this.newElem);
+
+                this.newElem = '';
+            }
+        }
+    }
+}).mount('#app');
